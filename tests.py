fc1aca13c12dd7b2fe04ae3f46a66e0aeca43d30
Emily Corso <ekcorso@gmail.com>
2022-12-10 09:57:36 -0800

    Add test for 'me' command

--- tests.py
+++ tests.py
@@ -3,3 +3,14 @@ import planet
 import player
 import item
 
+
+def test_me_command():
+    my_player = player.Player()
+
+    my_player.location = planet.Planet("Vulcan")
+    my_player.items = [item.Item("Rock", "This is just a rock")]
+
+    assert my_player.get_status() == "Health: 50. Location: Vulcan. Items: Rock.", "Status not working correctly"
+    my_player.items.append(item.Item("Phaser", "This is a badass phaser"))
+    assert my_player.get_status() == "Health: 50. Location: Vulcan. Items: Rock, Phaser.", "Status not working correctly"
+
