a4fb7dea057eeb36531c25d67ce2c918387b34eb
Emily Corso <ekcorso@gmail.com>
2022-12-11 20:42:44 -0800

    Add Weapon class and test

--- tests.py
+++ tests.py
@@ -14,3 +14,28 @@ def test_me_command():
     my_player.items.append(item.Item("Phaser", "This is a badass phaser"))
     assert my_player.get_status() == "Health: 50. Location: Vulcan. Items: Rock, Phaser.", "Status not working correctly"
 
+def test_weapon():
+    my_player = player.Player()
+    vulcan = planet.Planet("Vulcan")
+    my_player.location = vulcan
+
+    phaser = item.Weapon("Phaser", "This is a badass phaser", 5)
+    phaser.put_on_planet(vulcan)
+    my_player.pickup(phaser)
+    
+    quark = alien.Alien(
+        "Quark",
+        "Ferengi",
+        20,
+        vulcan,
+        False,
+        0.25,
+        "This species prizes business acumen. While Ferengi are not inherently hostile, they will persue profit at "
+        "all costs in most negotiations. Of course, it's hard not to miss thier very large ears too.",
+    )
+
+    phaser.fire(quark)
+    assert quark.health == 15, "Weapon not doing damage correctly"
+    assert phaser.amunition == 9, "Amunition is not depleting correctly"
+
+
