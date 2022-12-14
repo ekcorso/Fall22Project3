6016ae677046c599115d408858296ae6b6a3db64
Emily Corso <ekcorso@gmail.com>
2022-12-13 21:18:52 -0800

    Add tests for shields and pre-warp damage

--- tests.py
+++ tests.py
@@ -39,3 +39,74 @@ def test_weapon():
     assert phaser.amunition == 9, "Amunition is not depleting correctly"
 
 
+def test_attacking_pre_warp():
+    my_player = player.Player()
+    vulcan = planet.Planet("Vulcan")
+    my_player.location = vulcan
+
+    kes = alien.Alien(
+        "Kes",
+        "Ocampa",
+        15,
+        vulcan,
+        True,
+        0.25,
+        "The Ocampa are a humanoid, pre-warp species. After an accident damanged their planet's atmosphere they were"
+        "underground and only survived with the help of the Caretaker, a benevolent member of a more advanced species."
+    )
+
+    daggin = alien.Alien(
+        "Daggin",
+        "Ocampa",
+        15,
+        vulcan,
+        True,
+        0.25,
+        "The Ocampa are a humanoid, pre-warp species. After an accident damanged their planet's atmosphere they were"
+        "underground and only survived with the help of the Caretaker, a benevolent member of a more advanced species."
+    ) 
+
+    pre_warp_penalty = lambda: .95
+    no_pre_warp_penalty = lambda: .5
+
+    my_player.attack_alien(kes, pre_warp_penalty)
+    assert vulcan.get_alien_by_name(kes.name) is False, "Kes didn't die"
+    assert my_player.health == 80, "Something is wrong with damage assessment with pre warp penality"
+    my_player.attack_alien(daggin, no_pre_warp_penalty)
+    assert vulcan.get_alien_by_name(daggin.name) is False, "Daggin didn't die"
+    assert my_player.health == 65, "Something is wrong with damage assessment with no pre warp penality"
+
+
+def test_shields_raised():
+    my_player = player.Player()
+    vulcan = planet.Planet("Vulcan")
+    my_player.location = vulcan
+
+    quark = alien.Alien(
+        "Quark",
+        "Ferengi",
+        20,
+        vulcan,
+        False,
+        0.25,
+        "This species prizes business acumen. While Ferengi are not inherently hostile, they will persue profit at "
+        "all costs in most negotiations. Of course, it's hard not to miss thier very large ears too.",
+    )
+
+    nog = alien.Alien(
+        "Nog",
+        "Ferengi",
+        20,
+        vulcan,
+        False,
+        0.25,
+        "This species prizes business acumen. While Ferengi are not inherently hostile, they will persue profit at "
+        "all costs in most negotiations. Of course, it's hard not to miss thier very large ears too.",
+    )
+
+    my_player.raise_shields()
+    my_player.attack_alien(quark)
+    assert my_player.health == 90, "Incorrect damage after raising shields"
+    my_player.lower_shields()
+    my_player.attack_alien(nog)
+    assert my_player.health == 70, "Incorrect damange with shields lowered"
